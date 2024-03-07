05ba9bda938683b4ca2555a6adb1cb3d8a099c99
Brandao <Brandao@git.uoutec.com.br>
2024-03-07 15:44:03 -0300

    default

--- src/test/java/br/com/uoutec/community/ediacaran/test/ApplicationCreatorBootstrap.java
+++ src/test/java/br/com/uoutec/community/ediacaran/test/ApplicationCreatorBootstrap.java
@@ -8,10 +8,12 @@ import br.com.uoutec.ediacaran.core.ApplicationStart;
 public class ApplicationCreatorBootstrap {
 
 	public static void main(String[] vars) throws IOException, URISyntaxException {
+		/*
 		System.setProperty("system.security.policy", "C:/ediacaran/config/security.json");
 		System.setProperty("system.security", "true");
 		System.setProperty("system.security.print", "true");
 		System.setProperty("system.security.debug", "error");
+		*/
 		ApplicationStart.main(vars); 
 
 	}
