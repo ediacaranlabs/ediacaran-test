a0cbc1b1bc5c05637b6b120c0c8cceca1877e19e
Brandao <Brandao@git.uoutec.com.br>
2024-03-08 05:40:28 -0300

    default

---
+++ src/test/java/br/com/uoutec/community/ediacaran/test/ApplicationBootstrap.java
@@ -0,0 +1,11 @@
+package br.com.uoutec.community.ediacaran.test;
+
+import br.com.uoutec.ediacaran.core.ApplicationStart;
+
+public class ApplicationBootstrap {
+
+	public static void main(String[] vars) {
+		ApplicationStart.main(vars); 
+	}
+	
+}
