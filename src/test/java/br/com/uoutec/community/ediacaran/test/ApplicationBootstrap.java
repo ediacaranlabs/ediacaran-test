6da24c9ca05d9ee811dc0624e9f96501668bde23
Brandao <Brandao@git.uoutec.com.br>
2024-04-14 15:01:58 -0300

    default

--- src/test/java/br/com/uoutec/community/ediacaran/test/ApplicationBootstrap.java
+++ src/test/java/br/com/uoutec/community/ediacaran/test/ApplicationBootstrap.java
@@ -3,7 +3,7 @@ package br.com.uoutec.community.ediacaran.test;
 import br.com.uoutec.ediacaran.core.ApplicationStart;
 
 public class ApplicationBootstrap {
-	//br.com.uoutec.community.ediacaran.front.components.IfTagComponent
+	
 	public static void main(String[] vars) {
 		ApplicationStart.main(vars); 
 	}
